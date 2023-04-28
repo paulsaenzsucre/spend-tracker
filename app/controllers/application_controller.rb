b670f091a61ef4204701f71b781b0c17795ba571
Paul Sáenz Sucre <paulsaenzsucre@outlook.com>
2023-04-28 02:45:23 -0500

    Add helper method to prepare navbar content.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :set_navbar_content
+
+  def set_navbar_content
+    @navbar_content = {
+      'title' => 'MY APP',
+      'action_name' => 'Home',
+      'form_id' => 'form'
+    }
+  end
 end
