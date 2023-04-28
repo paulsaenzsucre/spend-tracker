3cac459ef5ae6878bb604be44445420463edb927
Paul Sáenz Sucre <paulsaenzsucre@outlook.com>
2023-04-28 12:46:25 -0500

    Add constant to manage navabar content.

---
+++ config/initializers/navbar.rb
@@ -0,0 +1,84 @@
+NAVBAR_CONTENT = {
+  'splashscreen' => {
+    'index' => {
+      'title' => 'SPLASHSCREEN',
+      'action_name' => 'Next',
+      'action' => 'new',
+      'form_id' => 'new_user'
+    }
+  },
+  'devise/registrations' => {
+    'new' => {
+      'title' => 'REGISTER',
+      'action_name' => 'Next',
+      'action' => 'new',
+      'form_id' => 'new_user'
+    },
+    'edit' => {
+      'title' => 'UPDATE PROFILE',
+      'action_name' => 'Update',
+      'action' => 'edit',
+      'form_id' => 'edit_user'
+    },
+    'update' => {
+      'title' => 'UPDATE PROFILE',
+      'action_name' => 'Update',
+      'action' => 'edit',
+      'form_id' => 'edit_user'
+    }
+  },
+  'devise/sessions' => {
+    'new' => {
+      'title' => 'LOG IN',
+      'action_name' => 'Log In',
+      'action' => 'new',
+      'form_id' => 'new_user'
+    },
+    'create' => {
+      'title' => 'LOG IN',
+      'action_name' => 'Log In',
+      'action' => 'new',
+      'form_id' => 'new_user'
+    }
+  },
+  'devise/passwords' => {
+    'new' => {
+      'title' => 'SEND INSTRUCTIONS',
+      'action_name' => 'Send',
+      'action' => 'new',
+      'form_id' => 'new_user'
+    },
+    'edit' => {
+      'title' => 'UPDATE PASSWORD',
+      'action_name' => 'Change',
+      'action' => 'edit',
+      'form_id' => 'new_user'
+    },
+    'update' => {
+      'title' => 'UPDATE PASSWORD',
+      'action_name' => 'Change',
+      'action' => 'update',
+      'form_id' => 'edit_user'
+    },
+    'create' => {
+      'title' => 'SEND INSTRUCTIONS',
+      'action_name' => 'Change',
+      'action' => 'update',
+      'form_id' => 'edit_user'
+    }
+  },
+  'devise/confirmations' => {
+    'new' => {
+      'title' => 'CONFIRMATION',
+      'action_name' => 'Send',
+      'action' => 'new',
+      'form_id' => 'new_user'
+    },
+    'create' => {
+      'title' => 'UPDATE PROFILE',
+      'action_name' => 'Update',
+      'action' => 'edit',
+      'form_id' => 'edit_user'
+    }
+  },
+}
