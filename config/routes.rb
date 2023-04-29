fef5e5e45c28c6ec8e3a9281afff2fad1c898bad
Paul Sáenz Sucre <paulsaenzsucre@outlook.com>
2023-04-28 22:49:20 -0500

    Refactor project routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  resources :categories
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   devise_for :users
   
-  root 'splashscreen#index'
+  root 'categories#index'
   get '/splashscreen', to: 'splashscreen#index'
+  resources :categories, only: [:index, :new] do
+    resources :expenses, only: [:index]
+  end
 end
