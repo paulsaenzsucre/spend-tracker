41ace4320fe3997a0526d9379a2b2bf81ce00c81
Paul Sáenz Sucre <paulsaenzsucre@outlook.com>
2023-04-29 15:36:31 -0500

    Fix models.

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,9 @@
 class Category < ApplicationRecord
   belongs_to :user
   has_many :cat_exps
+  validates :name, presence: true, length: { maximum: 200 }
+  validates :icon, presence: true, length: { maximum: 50 }
+  validates :user, presence: true
 
   def total_amount
     cat_exps.joins(:expense).sum(:amount)
--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,4 +1,6 @@
 class Expense < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :cat_exps
+  validates :name, presence: true, length: { maximum: 200 }
+  validates :amount, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
