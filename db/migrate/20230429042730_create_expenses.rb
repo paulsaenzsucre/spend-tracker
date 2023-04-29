777b07c845264dd09e05fb51fef051e3850adcae
Paul Sáenz Sucre <paulsaenzsucre@outlook.com>
2023-04-29 05:46:04 -0500

    Add expenses and cat-exp migrations

--- db/migrate/20230429042730_create_expenses.rb
+++ db/migrate/20230429042730_create_expenses.rb
@@ -1,7 +1,7 @@
 class CreateExpenses < ActiveRecord::Migration[7.0]
   def change
     create_table :expenses do |t|
-      t.references :user, null: false, foreign_key: true
+      t.references :author, foreign_key: {to_table: :users}
       t.string :name
       t.decimal :amount
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_28_180712) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_29_043525) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cat_exps", force: :cascade do |t|
+    t.bigint "category_id", null: false
+    t.bigint "expense_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_cat_exps_on_category_id"
+    t.index ["expense_id"], name: "index_cat_exps_on_expense_id"
+  end
 
   create_table "categories", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -21,6 +30,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_28_180712) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_categories_on_user_id"
+  end
+
+  create_table "expenses", force: :cascade do |t|
+    t.bigint "author_id"
+    t.string "name"
+    t.decimal "amount"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_expenses_on_author_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -42,5 +60,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_28_180712) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "cat_exps", "categories"
+  add_foreign_key "cat_exps", "expenses"
   add_foreign_key "categories", "users"
+  add_foreign_key "expenses", "users", column: "author_id"
 end
