778288405a3866d3d3b6d0ec909c431db196115d
Paul Sáenz Sucre <paulsaenzsucre@outlook.com>
2023-04-29 05:54:08 -0500

    Refactor routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :cat_exps
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
@@ -7,6 +8,7 @@ Rails.application.routes.draw do
   root 'categories#index'
   get '/splashscreen', to: 'splashscreen#index'
   resources :categories, only: [:index, :new] do
-    resources :expenses, only: [:index]
+    resources :cat_exps, only: [:index, :new, :create]
   end
+  resources :expenses, only: [:new, :create]
 end
